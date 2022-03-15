dbc545e432585631a2ea9db688516a450ce8bccd
Gustavo Oliveira <gustavooliveira>
2022-03-15 16:39:01 -0300

    Ajustando a entrada de dados

--- Primeiros passos.R
+++ Primeiros passos.R
@@ -13,7 +13,7 @@
 #Importação de dados
 #Há várias maneiras de exportar seus dados. Aqui usaremos o pacote Rio para essa função.
 
-#install.packages("rio")
+install.packages("rio")
 library(rio)
 
 #Antes de importar seus dados, tenha certeza que setou o caminho correto 
@@ -24,7 +24,16 @@ setwd("CAMINHO DOS DADOS")
 #Agora você está pronto para importar seus dados com muita facilidade e por meio de linhas de 
 #comando.
 
-rio::import("DADOS")#Quando se usa pacote::função é a mesma coisa de usar somente a função, mas antes você deve acionar o labrary()
+rio::import("DADOS.csv")#Usar dessa forma quando não acionar a função labrary()
+#ou somente
+import("DADOS.csv")#Usar dessa forma quando acionar o library(rio)
+
+#----ATENÇÃO----#
+#É importante sempre direcionar os dados importados a algum objeto dentro do R, 
+#para que facilite sua manipulação. Não é obrigatório, mas é uma boa sugestão
+
+#Exemplor
+DADOS <- import("DADOS.csv")
 
 
 
